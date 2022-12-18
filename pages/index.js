83400410e43727ab805cb727f6fd9f4afc0eab9a
John <codebeast0420@gmail.com>
2022-12-18 13:43:29 +0800

    tailwind fix

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import Header from '../src/component/header'
+import Header from '../component/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
